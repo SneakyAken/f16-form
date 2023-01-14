5b30bdbd00213dac57ca688a3eecdeecc2689779
SneakyAken <spaccosgm@gmail.com>
2023-01-14 21:05:34 +0100

    style

---
+++ style.php
@@ -0,0 +1,36 @@
+<?php
+header("Content-type: text/css")
+.box1 {
+    padding: 20px;
+    width: 500px;
+    background-color: rgba(0, 77, 128, 50%);
+    position: absolute;
+    left: 15%;
+    text-align: center;
+    top: 20%;
+    font-family: Geneva;
+    font-size: 20px;
+    color: white;
+    border-radius: 10px;
+    text-shadow: 0px 1px black;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
